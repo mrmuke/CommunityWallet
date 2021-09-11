a72a7144258e9bec9fb00f0d82ccce4d1462cba1
ezzatisawesome <ezzatisawesome@gmail.com>
2021-09-11 12:50:38 -0500

    working on mongo

---
+++ realmDev.js
@@ -0,0 +1,66 @@
+import Realm from "realm";
+import { UUID } from 'bson';
+
+// Returns the shared instance of the Realm app.
+export function getRealmApp() {
+    const appId = 'walletdevrealm-khyji'; // Set Realm app ID here.
+    const appConfig = {
+        id: appId,
+        timeout: 10000,
+    };
+    return new Realm.App(appConfig);
+}
+
+export const WalletSchema = {
+    name: 'Wallet',
+    properties: {
+        _id: 'uuid',
+        _partition: 'uuid',
+        phoneNumber: 'int',
+    },
+    primaryKey: '_id',
+};
+
+async function anonymousLogin() {
+    let user;
+    try {
+        const app = getRealmApp(); // pass in the appConfig variable that you created earlier
+        const credentials = Realm.Credentials.anonymous(); // create an anonymous credential
+        user = await app.logIn(credentials);
+        return user;
+    } catch (error) {
+        throw `Error logging in anonymously: ${JSON.stringify(error, null, 2)}`;
+    }
+}
+
+let partition_id = new UUID()
+
+async function openRealm() {
+    let user = await anonymousLogin();
+    let realm;
+    try {
+        // ...
+        console.log(`Logged in with the user: ${user.identity}`);
+        const config = {
+            schema: [WalletSchema],
+            sync: {
+                user: user,
+                partitionValue: partition_id
+            },
+        };
+        realm = await Realm.open(config);
+        return realm
+    } catch (error) {
+        throw `Error opening realm: ${JSON.stringify(error, null, 2)}`;
+    }
+}
+
+const realm = await openRealm()
+
+realm.write(() => {
+    realm.create("Wallet", {
+        _id: new UUID(),
+        _partition: partition_id,
+        phoneNumber: 2248068224
+    });
+});
