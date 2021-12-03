95b4d6e2980511d7b3e289f4f8048993fa6b6838
Michael <m.xingster@gmail.com>
2021-12-03 17:13:23 -0600

    hi

--- API_URL.js
+++ API_URL.js
@@ -3,5 +3,5 @@ const { manifest } = Constants;
 
 const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev
   ? "http://"+manifest.debuggerHost.split(`:`).shift().concat(`:3000`)
-  : `api.example.com`;
+  : `http://3.101.141.11:3000`;
   export default api
