779e5635a1f9b79d9abb71bdf05d6a74673e0d9a
ezzatisawesome <ezzatsuhaime@gmail.com>
2022-06-13 21:50:54 -0500

    Restart

--- App.js
+++ App.js
@@ -1,40 +1,25 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
-import Login from './screens/Login'
-import Signup from './screens/Signup'
-import { NativeBaseProvider } from 'native-base';
-import Drawer from './screens/Drawer'
-import { NavigationContainer } from '@react-navigation/native';
-import firebase from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBIwcuAX4RLMczJFAhlrVechxp9EEn7szQ",
-  authDomain: "communitywallet-ed36c.firebaseapp.com",
-  projectId: "communitywallet-ed36c",
-  storageBucket: "communitywallet-ed36c.appspot.com",
-  messagingSenderId: "697161665894",
-  appId: "1:697161665894:web:83280e7c5a650375448ca7",
-  measurementId: "G-5E83HSB3X0"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <NativeBaseProvider>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Drawer" component={Drawer}/>
-      </Stack.Navigator></NativeBaseProvider>
-    </NavigationContainer>
-  );
-
+    <View style={styles.container}>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={ styles.testImage } />
+      <Text style={{ color: 'blue', fontSize: '20' }}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  testImage: {
+    width: 305,
+    height: 150
+  },
+})
